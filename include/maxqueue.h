d96a31c872c101c19b6999daa30704c71ab68d1c
li <liyaojian0811@gmail.com>
2024-10-28 13:36:00 +0100

    maxqueue.* escrito sin comentario

--- include/maxqueue.h
+++ include/maxqueue.h
@@ -7,26 +7,48 @@
 #ifndef MAXQUEUE_H
 #define MAXQUEUE_H
 
+#include <iostream>
 #include <list>
 #include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/* @brief Estructura auxiar para el retorno del par valor-maximo
+ *
+ * Para un par en un momento dado representan:
+ * - valor: el retorno de la estructura semejante de la stl
+ * - maximo: el máximo de la estructura en cierto momento
+ *
+ */
+struct element{
+ int valor;
+ int maximo;
+};
+
+/**
+ * @brief Sobrecarga del operador de salida de un element
+ * El resultado en @p os será de la forma: v | m
+ * Dónde v es el valor y m el máximo
+ */
+ostream& operator<<(ostream& os, const element& elemento);
+
 
 class MaxQueue
 {
  private:
- std::list<int> queue;
- std::list<int> maximo;
+  list<int> queue;
+  int maximo;
  public:
- MaxQueue();
- ~MaxQueue();
- bool empty();
- int size();
- MaxQueue front();
- MaxQueue back();
- void emplace();
- void pop();
- void swap();
- friend std::ostream &operator<<(std::ostream &os, const MaxQueue &queue);
+  MaxQueue();
+  ~MaxQueue();
+  bool empty() const;
+  int size() const;
+  element front() const;
+  element back() const;
+  void pop();
+  void push(int num);
+  void swap(MaxQueue & another_queue);
 };
 
-
-#endif MAXQUEUE_H
+#endif
--- src/maxqueue.cpp
+++ src/maxqueue.cpp
@@ -3,3 +3,49 @@
  * @brief  Archivo de implementación del TDA MaxQueue
  * @author
  */
+
+#include "maxqueue.h"
+
+MaxQueue::MaxQueue()
+{
+ maximo = numeric_limits<int>::min();
+}
+MaxQueue::~MaxQueue() = default;
+bool MaxQueue::empty() const
+{
+ return queue.empty();
+}
+int MaxQueue::size() const
+{
+ return static_cast<int>(queue.size());
+}
+element MaxQueue::front() const
+{
+ return element{queue.front(),maximo};
+}
+element MaxQueue::back() const
+{
+ return element{queue.back(),maximo};
+}
+void MaxQueue::pop()
+{
+ if (queue.front() == maximo)
+  maximo = *max_element(queue.begin(),queue.end());
+ queue.pop_front();
+}
+void MaxQueue::push(int num)
+{
+ if (num > maximo)
+  maximo = num;
+ queue.push_back(num);
+}
+void MaxQueue::swap(MaxQueue & another_queue)
+{
+ queue.swap(another_queue.queue);
+ int temp = maximo;
+ maximo = another_queue.maximo;
+ another_queue.maximo = temp;
+}
+
+
+
